cf2b9bf066d93f21f1cdb8a529f2482e33e77d80
aseg3092 <gabrielsalazar3092@gmail.com>
2022-04-02 00:53:25 +0000

    bd con la ross y el uruguayo

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,8 @@ class User(db.Model):
     lastName = db.Column(db.String(120),unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), unique=False, nullable=False)
-    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    orders = db.relationship('OrderCart', backref='user', lazy=True)
+    address = db.relationship('Address', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -23,8 +24,13 @@ class User(db.Model):
 class OrderCart(db.Model):
     orderCartID = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
-    productID = db.Column(db.Integer, db.ForeignKey('person.id'),
+    TotalMount = db.Column(db.Integer,unique=False, nullable=True)
+    productID = db.Column(db.Integer, db.ForeignKey('product.productID'),
         nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+
+    
     
     def __repr__(self):
         return '<OrderCart %r>' % self.orderCartID
@@ -33,25 +39,9 @@ class OrderCart(db.Model):
         return {
             "orderCartID": self.orderCartID,
             "quantity": self.quantity,
-            "productID": self.productID
-        }
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=False, nullable=True)
-    lastName = db.Column(db.String(120),unique=False, nullable=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), unique=False, nullable=False)
-    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.email
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "productID": self.productID,
+            "TotalMount": self.TotalMount,
+            "user_id": self.user_id
         }
 
 class Product(db.Model):
@@ -61,10 +51,11 @@ class Product(db.Model):
     description = db.Column(db.String(256), unique=True, nullable=False)
     category = db.Column(db.String(256), unique=False, nullable=False)
     image = db.Column(db.String(256), unique=False, nullable=False)
+    orders = db.relationship('OrderCart', backref='order', lazy=True)
     
 
     def __repr__(self):
-        return '<Product %r>' % self.title
+        return '<Product %r>' % self.productID
 
     def serialize(self):
         return {
@@ -73,5 +64,25 @@ class Product(db.Model):
             "price": self.price,
             "description": self.description,
             "category": self.category,
-            "image": self.image,
+            "image": self.image
         }
+
+
+
+class Address(db.Model):
+    addressID = db.Column(db.Integer, primary_key=True)
+    address1 = db.Column(db.String(120), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    
+
+    def __repr__(self):
+        return '<Address %r>' % self.addressID
+
+    def serialize(self):
+        return {
+            "addressID": self.addressID,
+            "address1": self.address1,
+            "user_id": self.user_id
+        }
+
